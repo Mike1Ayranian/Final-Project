8ae05bc2aecb5b7ca55ceac5d66e6e986014f79e
Mike1Ayranian <mayranian@gmail.com>
2017-05-17 10:16:22 -0700

    added stuff

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -31,7 +31,15 @@ if (typeof jQuery === 'undefined') {
   // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
   // ============================================================
   function justin() {
-   document.getElementById("banger1").setAttribute("style", "background: url(http://data.whicdn.com/images/112017850/large.jpg)")}
+  var number = Math.random();
+  if (number < 0.67){
+   document.getElementbyID("banger1").setAttribute("style", "background: url(http://data.whicdn.com/images/112017850/large.jpg)")}
+   else if (number < 0.33){
+  document.getElementbyID("banger1").setAttribute("style", "background: url(https://muzikspeaks.files.wordpress.com/2015/03/coldplay-banner.jpg)")}
+  else
+  document.getElementbyID("banger1").setAttribute("style", "background: url(http://www.onlinetickettrading.com/catimages/coldplay-banner.jpg)")
+  }
+
 
   function transitionEnd() {
     var el = document.createElement('bootstrap')
--- js/somethin.js
+++ js/somethin.js
@@ -1,3 +1,9 @@
 function justin() {
+var number = Math.random();
+if (number < 0.67){
  document.getElementbyID("banger1").setAttribute("style", "background: url(http://data.whicdn.com/images/112017850/large.jpg)")}
- 
+ else if (number < 0.33){
+document.getElementbyID("banger1").setAttribute("style", "background: url(https://muzikspeaks.files.wordpress.com/2015/03/coldplay-banner.jpg)")}
+else
+document.getElementbyID("banger1").setAttribute("style", "background: url(http://www.onlinetickettrading.com/catimages/coldplay-banner.jpg)")
+}
